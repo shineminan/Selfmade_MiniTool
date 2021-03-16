e118e38f2e1af576b32a25e6ec6adc45fe36177c
shineminan <shineminan@gmail.com>
2021-03-16 03:22:50 +0100

    show all images who has hist sum smaller than 0.5

--- FindSimilarImage.py
+++ FindSimilarImage.py
@@ -20,6 +20,16 @@ def load_images_from_folder(folder):
             images_dict[filename] = img
     return images_dict
 
+def combine_image_sbys(img1,img2):
+    h1, w1 = img1.shape[:2]
+    h2, w2 = img2.shape[:2]
+    h = max(h1, h2)
+    w = w1+w2
+    vis = np.zeros((h, w, 3), np.uint8)
+    vis[:h1, :w1,:3] = img1
+    vis[:h2, w1:w1+w2,:3] = img2
+    return h, w, vis
+
 folder = "C:\\Users\\shine\\Desktop\\Tom_Daley"
 imagesdict = load_images_from_folder(folder)
 imageslist = list(imagesdict.values())
@@ -32,16 +42,22 @@ for image in imageslist:
 i = 0
 # while i == 0:
 print("Checking " + str(i+1) + "/" + str(totalpicnumber) + " pictures")
-histdifflist = list(np.sum(abs(histlist - histlist[i]), 1))
 
+histdifflist = list(np.sum(abs(histlist - histlist[i]), 1))
 histdifflist[i] = 100
 c = sorted(histdifflist)
 print(c)
+smallhist = [x for x in c if x <= 0.5]
+print(smallhist)
 
-d = histdifflist.index(c[0])
+for item in smallhist:
+    print(item)
+    a = histdifflist.index(item)
+    h, w, possiblesimilarimages = combine_image_sbys(imageslist[0], imageslist[a])
+    imS = cv2.resize(possiblesimilarimages, (1000, int(1000/w*h)))
 
-possiblesimilarimages = np.hstack((imageslist[0], imageslist[d]))
+    cv2.imshow('image', imS)
+    cv2.waitKey(0)
 
 
-cv2.imshow('image', possiblesimilarimages)
-cv2.waitKey(0)
+
