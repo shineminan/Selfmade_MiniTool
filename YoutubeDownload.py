944a92229ed2393efa6467a1313a287a8b8ebcbb
shineminan <shineminan@gmail.com>
2021-03-24 02:09:07 +0100

    playlist selection downloading supported

--- YoutubeDownload.py
+++ YoutubeDownload.py
@@ -9,12 +9,10 @@ webm_path = saveto + "\Webms"
 audio_path = saveto + "\Audios"
 output_path = "E:\Video\eCloud\Videos"
 
-def playlistdownload(url,se):
-    urls = p.Playlist(url)
-    l = len(urls)
-    for x in range(se-1, l):
-        a = p.YouTube(urls[x])
-        i = x + 1
+def playlistdownload(urls,episodelist):
+    for i in episodelist: # i is the episode number
+        episodeindex = i - 1
+        a = p.YouTube(urls[episodeindex])
 
         t1 = time.perf_counter()
         print("Episode " + str(i) + " webm started downloading!")
@@ -37,9 +35,9 @@ def playlistdownload(url,se):
         t2 = time.perf_counter()
         min, sec = divmod(t2-t1, 60)
         print("Episode "+str(i)+"/"+str(l) +" finished! " + str(int(min)) + "Min" + str(int(sec)) + "Sec used")
+        playsound("./AudioFile/cartoon_bubble_pop.mp3")
         os.remove(webm_rename)
         os.remove(audio_rename)
-        playsound("./AudioFile/cartoon_bubble_pop.mp3")
 
 def singlevidoedownload(url):
     a = p.YouTube(url)
@@ -66,19 +64,35 @@ def singlevidoedownload(url):
     os.remove(audio_rename)
     thename = audio.replace("E:\\Video\\eCloud\\Audios\\", "").replace(".webm", "")
     print("DONE! " + str(int(min)) + "Min" + str(int(sec)) + "Sec used And the original video title is: " + thename)
-    playsound("./AudioFile/cartoon_mallets_rise_up_fast_2_stephttps://youtu.be/qr3dbMSsyCMs.mp3")
+    playsound("./AudioFile/cartoon_mallets_rise_up_fast_2_steps.mp3")
 
 playlisttext = "playlist"
 givenurl = input("Paste your URL here:")
 if playlisttext in givenurl:
-    se = input("Which episode you want to start from? ")
-    se = min(se, 1)
+    urls = p.Playlist(givenurl)
+    l = len(urls)
+
+    data = input("Which episodes you want to download? Input ike: (0-1000), (2-8), (0-8), (2-1000),(2,4,6,8) -> ")
+    if "-" in data:
+        mid = data.index("-")
+        startepisode = max(int(data[:mid]), 1)
+        endepisode = min(int(data[mid+1:]), l)
+        episodelist = [x for x in range(startepisode,endepisode+1)]
+        print(episodelist)
+    elif "," in data:
+        inputlist = [int(s) for s in data.split(",") if s.isdigit()]
+        episodelist = [x for x in inputlist if 0 < x <= l]
+        if len(episodelist) < inputlist:
+            print("Hey, some episode(s) are out of range, but I wont tell which, hehe ")
+    else:
+        print("Duck you, totally wrong input!!!")
+
     a = time.perf_counter()
     print("It is a playlist, will take a long time to download all")
-    playlistdownload(givenurl, int(se))
+    playlistdownload(urls, episodelist)
     b = time.perf_counter()
     min, sec = divmod(b - a, 60)
-    print("ALL DONE! Totally "+ str(int(min)) + "Min" + str(int(sec)) + "Sec used")
+    print("ALL DONE! Totally "+ str(min) + "Min" + str(sec) + "Sec used")
     playsound("./AudioFile/cartoon_mallets_rise_up_fast_2_steps.mp3")
 else:
     print("Just one video, will be finished soon")
