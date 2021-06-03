d849d4392cb9591af26650757457630076938aea
shineminan <shineminan@gmail.com>
2021-06-03 20:07:23 +0200

    find similar image in a folder woking now, yeah!

--- FindSimilarImage.py
+++ FindSimilarImage.py
@@ -2,11 +2,13 @@ import numpy as np
 import cv2
 import os
 import ctypes
-import shutil
+user32 = ctypes.windll.user32
+screenw, screenh = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
+sensitivity = 0.3
 
 
 def return_his(pic):
-    a, _ = np.histogram(pic, 64, [0,256])
+    a, _ = np.histogram(pic, 64, [0, 256])
     b = a/sum(a)
     return b
 
@@ -22,16 +24,6 @@ def load_images_from_folder(folder):
             images_dict[filename] = img
     return images_dict
 
-# def show_image_sbys(img1,img2):
-#     h1, w1 = img1.shape[:2]
-#     h2, w2 = img2.shape[:2]
-#     h = max(h1, h2)
-#     w = w1+w2
-#     vis = np.zeros((h, w, 3), np.uint8)
-#     vis[:h1, :w1,:3] = img1
-#     vis[:h2, w1:w1+w2,:3] = img2
-#     return h, w, vis
-
 def show_image_h(lst): #show images horizontally
     hs = []
     ws = []
@@ -45,7 +37,6 @@ def show_image_h(lst): #show images horizontally
     for i in range(len(lst)):
         hi = hs[i]
         wi = ws[i]
-        # print("imagesgaoe",hi,wi)
         vis[:hi, w:w+wi, :3] = lst[i]
         w += wi
 
@@ -56,50 +47,50 @@ def show_image_h(lst): #show images horizontally
         displayw = int(screenh/max(hs)*sum(ws))
 
     imS = cv2.resize(vis, (displayw, displayh))
-
-
     cv2.imshow('image', imS)
-    cv2.waitKey(0)
-
-
-
-
-
-
-
-
-user32 = ctypes.windll.user32
-screenw, screenh = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
-
-
+    a = cv2.waitKey(0)
+    return a
 
 
 
 
 folder = input("Paste the folder address here: ")
 imagesdict = load_images_from_folder(folder)
+filenamelist = list(imagesdict.keys())
 imageslist = list(imagesdict.values())
 totalpicnumber = len(imageslist)
+deletenamelst = []
+
 histlist = []
 for image in imageslist:
     histlist.append(return_his(image))
 
 for i in range(totalpicnumber):
-    print("Checking " + str(i+1) + "/" + str(totalpicnumber) + " pictures")
+    print("Checking " + "picture: " + filenamelist[i] + " -> (" + str(i+1) + "/" + str(totalpicnumber) + ")")
 
     histdifflist = list(np.sum(abs(histlist - histlist[i]), 1))
     histdifflist[i] = 100
-
-    c = sorted(histdifflist)
-    # print("sorted hist", c)
-
-    smallhist = [x for x in histdifflist if x <= 0.3]
+    # c = sorted(histdifflist)
+    smallhist = [x for x in histdifflist[i:] if x <= sensitivity]
 
     similarimages = []
+    similarimages_filename = []
     if len(smallhist) == 0:
         print("Didn't find a similar image")
     else:
-        similarimages.append(imageslist[histdifflist.index(100)])
+        a = histdifflist.index(100)
+        similarimages.append(imageslist[a])
+        similarimages_filename.append(filenamelist[a])
         for diff in smallhist:
-            similarimages.append(imageslist[histdifflist.index(diff)])
-        show_image_h(similarimages)
+            a = histdifflist.index(diff)
+            similarimages.append(imageslist[a])
+            similarimages_filename.append(filenamelist[a])
+        print(similarimages_filename)
+        index = int(show_image_h(similarimages))
+        if index > 48:
+            number = index - 48
+            deletenamelst.append(similarimages_filename[number-1])
+
+for name in tuple(deletenamelst):
+    os.remove(folder + "\\" + name)
+print("Totally " + str(len(deletenamelst)) + " pictures deleted")
