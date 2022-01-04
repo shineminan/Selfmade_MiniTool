3d073b1cf5a879b915b1d4dd45cdac4c4b6da508
shineminan <shineminan@gmail.com>
2022-01-05 00:04:07 +0100

    can search keyword in specifuc range of the file name, currently only the latery part of the name, not any range

--- KeywordDeletion.py
+++ KeywordDeletion.py
@@ -1,12 +1,16 @@
 import os
 
-def single_folder_deletion(folder, keyword):
-    print(list(folder.split("\\"))[-1] + " is being check")
+def single_folder_deletion(folder, keyword,loc):
+    x, y = loc.replace("(", "").replace(")", "").split(":")
     deletelist = []
     notcontain = 0
     contain = 0
+    print(list(folder.split("\\"))[-1] + " is being check")
+
     for filename in os.listdir(folder):
-        if keyword in filename:
+        purefilename = list(filename.rsplit(".", 1))[0]
+        aa = purefilename[int(x):]
+        if keyword in purefilename:
             contain += 1
             deletelist.append(filename)
         else:
@@ -19,7 +23,7 @@ def single_folder_deletion(folder, keyword):
             os.remove(folder + "\\" + deletename)
         print("Totally " + str(contain) + " files are deleted")
 
-def multiple_folder_deletion(folder, keyword):
+def multiple_folder_deletion(folder, keyword,loc):
     subfolders = os.listdir(folder)
     paths_list = []
     for name in subfolders:
@@ -27,16 +31,23 @@ def multiple_folder_deletion(folder, keyword):
         paths_list.append(subfolderpath)
     for path in paths_list:
         print("." * 200)
-        single_folder_deletion(path, keyword)
+        single_folder_deletion(path, keyword, loc)
     print("All Done!")
 
-infoinput = input("Paste the folder address here seperated by a coma with indication of single folder deletion(1) or multiple deletion(2): ")
-folder, keyword, index = (infoinput.split(","))
+
+infoinput = input("Paste the folder address; The keyword to search; The index of location of the keyword, eg: (:5) ,(5-10) , (10:); Single folder deletion(1) or multiple deletion(2):   ")
+folder, keyword, loc, index = infoinput.split(",")
+# x, y = loc.replace("(", "").replace(")", "").split(":")
+#
+# print(x, y)
+#
+
+
 if int(index) == 1:
-    single_folder_deletion(folder, keyword)
+    single_folder_deletion(folder, keyword,loc)
 elif int(index) == 2:
     multiple_folder_deletion(folder, keyword)
 else:
     print("Duck you, totally wrong input!!!")
 
-    # C:\Users\shine\Desktop\aa, .1, 2
+    # C:\Users\shine\Desktop\Levi_Conley,.1,(-2:),1
